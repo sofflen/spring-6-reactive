b995354725dac1d6adfe795914564b9b855adc61
sofflen <nixxesgaming@gmail.com>
2024-09-07 21:51:43 +0300

    added more unit tests

--- src/test/java/com/study/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/com/study/spring6reactive/controllers/BeerControllerTest.java
@@ -1,7 +1,6 @@
 package com.study.spring6reactive.controllers;
 
 import com.study.spring6reactive.model.BeerDTO;
-import com.study.spring6reactive.utils.TestUtils;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -15,6 +14,7 @@ import reactor.core.publisher.Mono;
 
 import static com.study.spring6reactive.controllers.BeerController.BEER_ID_PATH;
 import static com.study.spring6reactive.controllers.BeerController.BEER_PATH;
+import static com.study.spring6reactive.utils.TestUtils.getTestBeer;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -45,28 +45,88 @@ class BeerControllerTest {
     }
 
     @Test
+    @Order(2)
+    void testGetBeerByIdNotFound() {
+        webClient.get().uri(BEER_ID_PATH, 999)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
     @Order(3)
     void testCreateBeer() {
         webClient.post().uri(BEER_PATH)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(TestUtils.getTestBeer()), BeerDTO.class)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectHeader().location("http://localhost:8080/api/v2/beers/4");
     }
 
     @Test
+    @Order(3)
+    void testCreateBeerBadRequest() {
+        var testBeer = getTestBeer();
+        testBeer.setBeerName("");
+
+        webClient.post().uri(BEER_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(testBeer), BeerDTO.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
     @Order(4)
     void testUpdateBeer() {
-        var testBeer = TestUtils.getTestBeer();
-        String updatedName = "updated";
-        testBeer.setBeerName(updatedName);
+        webClient.put().uri(BEER_ID_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    @Order(4)
+    void testUpdateBeerNotFound() {
+        webClient.put().uri(BEER_ID_PATH, 999)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(4)
+    void testUpdateBeerBadRequest() {
+        var testBeer = getTestBeer();
+        testBeer.setBeerName("");
 
         webClient.put().uri(BEER_ID_PATH, 1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(testBeer), BeerDTO.class)
                 .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(5)
+    void testPatchBeer() {
+        webClient.patch().uri(BEER_ID_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .exchange()
                 .expectStatus().isNoContent();
+    }
+
+    @Test
+    @Order(5)
+    void testPatchBeerNotFound() {
+        webClient.patch().uri(BEER_ID_PATH, 999)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
     }
 
     @Test
@@ -77,4 +137,11 @@ class BeerControllerTest {
                 .expectStatus().isNoContent();
     }
 
+    @Test
+    @Order(999)
+    void testDeleteBeerNotFound() {
+        webClient.delete().uri(BEER_ID_PATH, 999)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
 }
--- src/test/java/com/study/spring6reactive/controllers/CustomerControllerTest.java
+++ src/test/java/com/study/spring6reactive/controllers/CustomerControllerTest.java
@@ -1,7 +1,6 @@
 package com.study.spring6reactive.controllers;
 
 import com.study.spring6reactive.model.CustomerDTO;
-import com.study.spring6reactive.utils.TestUtils;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -15,6 +14,7 @@ import reactor.core.publisher.Mono;
 
 import static com.study.spring6reactive.controllers.CustomerController.CUSTOMER_ID_PATH;
 import static com.study.spring6reactive.controllers.CustomerController.CUSTOMER_PATH;
+import static com.study.spring6reactive.utils.TestUtils.getTestCustomer;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -45,28 +45,88 @@ class CustomerControllerTest {
     }
 
     @Test
+    @Order(2)
+    void testGetCustomerByIdNotFound() {
+        webClient.get().uri(CUSTOMER_ID_PATH, 999)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
     @Order(3)
     void testCreateCustomer() {
         webClient.post().uri(CUSTOMER_PATH)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(TestUtils.getTestCustomer()), CustomerDTO.class)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectHeader().location("http://localhost:8080/api/v2/customers/4");
     }
 
     @Test
+    @Order(3)
+    void testCreateCustomerBadRequest() {
+        var testCustomer = getTestCustomer();
+        testCustomer.setCustomerName("");
+
+        webClient.post().uri(CUSTOMER_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(testCustomer), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
     @Order(4)
     void testUpdateCustomer() {
-        var testCustomer = TestUtils.getTestCustomer();
-        String updatedName = "updated";
-        testCustomer.setCustomerName(updatedName);
+        webClient.put().uri(CUSTOMER_ID_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNoContent();
+    }
+
+    @Test
+    @Order(4)
+    void testUpdateCustomerNotFound() {
+        webClient.put().uri(CUSTOMER_ID_PATH, 999)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    @Order(4)
+    void testUpdateCustomerBadRequest() {
+        var testCustomer = getTestCustomer();
+        testCustomer.setCustomerName("");
 
         webClient.put().uri(CUSTOMER_ID_PATH, 1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(testCustomer), CustomerDTO.class)
                 .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @Order(5)
+    void testPatchCustomer() {
+        webClient.patch().uri(CUSTOMER_ID_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
+                .exchange()
                 .expectStatus().isNoContent();
+    }
+
+    @Test
+    @Order(5)
+    void testPatchCustomerNotFound() {
+        webClient.patch().uri(CUSTOMER_ID_PATH, 999)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(getTestCustomer()), CustomerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
     }
 
     @Test
@@ -77,4 +137,11 @@ class CustomerControllerTest {
                 .expectStatus().isNoContent();
     }
 
+    @Test
+    @Order(999)
+    void testDeleteCustomerNotFound() {
+        webClient.delete().uri(CUSTOMER_ID_PATH, 999)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
 }
