79825117eb3a225a8dae4f292c8bb146871341ad
sofflen <nixxesgaming@gmail.com>
2024-09-11 13:00:24 +0300

    configured spring security

---
+++ src/main/java/com/study/spring6reactive/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.study.spring6reactive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(security ->
+                        security.anyExchange().authenticated())
+                .oauth2ResourceServer(server ->
+                        server.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
